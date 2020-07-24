b2e412a39a9b17ed641ea8cd67d9a2df12819f31
Nik Krein <nikkrein1@gmail.com>
2020-07-24 23:41:18 +0300

    Refactor SubmitForFacultyButton

--- src/main/java/by/epam/krein/abitapp/controller/commandImpl/SubmitForFacultyButton.java
+++ src/main/java/by/epam/krein/abitapp/controller/commandImpl/SubmitForFacultyButton.java
@@ -7,43 +7,31 @@ import by.epam.krein.abitapp.exception.CommandException;
 import by.epam.krein.abitapp.service.SpecialtyService;
 import by.epam.krein.abitapp.service.ServiceFactory;
 import by.epam.krein.abitapp.service.UserService;
+import by.epam.krein.abitapp.util.HttpUtils;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.sql.SQLException;
 
 public class SubmitForFacultyButton implements Command {
 
     ServiceFactory serviceFactory = ServiceFactory.getInstance();
     UserService userService = serviceFactory.getUserService();
-    SpecialtyService specialtyService = serviceFactory.getSpecialtyService();
-
-    //private final UserService userService = new UserServiceImpl(); // fabrika potom
-    //private final SpecialtyService facultyService = new SpecialtyServiceImpl(); // fa
 
 
     @Override
-    public CommandName callCommandMethod(HttpServletRequest req, HttpServletResponse resp) {
-
-        HttpSession session = req.getSession();
-        User user = (User) session.getAttribute("user");
-
+    public CommandName callCommandMethod(HttpServletRequest req) {
+        User user = (User) req.getSession().getAttribute("user");
         int id = Integer.parseInt(req.getParameter("id"));
         int formOfTraining = Integer.parseInt(req.getParameter("formOfTraining"));
 
         try {
             userService.updateUserRequest(id, formOfTraining, user.getId());
-        } catch(RuntimeException exception){
+            HttpUtils.updateSession(req, "user", userService.findUserById(user.getId()));
+        } catch (RuntimeException exception) {
             throw new CommandException("message", exception);
         }
 
-        try {
-            user = userService.findUserById(user.getId());
-            req.getSession().setAttribute("user", user);
-            return CommandName.PROFILE;
-        } catch(RuntimeException exception){
-            throw new CommandException("message", exception);
-        }
+        return CommandName.PROFILE;
     }
+
 }
