864ef9bdcd18b7e53fc1204fcd06a9aa55808d09
Nik Krein <nikkrein1@gmail.com>
2020-07-25 14:03:09 +0300

    Refactor signUp class and jsp page

--- src/main/java/by/epam/krein/abitapp/util/HttpUtils.java
+++ src/main/java/by/epam/krein/abitapp/util/HttpUtils.java
@@ -9,4 +9,14 @@ public class HttpUtils {
     public static<T> void updateSession(HttpServletRequest req, String sessionAttributeName, T sessionAttribute){
         req.getSession().setAttribute(sessionAttributeName, sessionAttribute);
     }
+
+    public static boolean isMethodPost(HttpServletRequest req) {
+        return req.getMethod().equalsIgnoreCase("post");
+    }
+
+    public static boolean isMethodGet(HttpServletRequest req) {
+        return req.getMethod().equalsIgnoreCase("get");
+    }
+
+    //public static boolean
 }
