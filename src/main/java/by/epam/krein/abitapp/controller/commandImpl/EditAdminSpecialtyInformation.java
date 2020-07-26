df153a42f127e2909f638de53a87523e4b3bb0ae
Nik Krein <nikkrein1@gmail.com>
2020-07-27 02:21:45 +0300

    Refactor EditAdminSpecialtyInformation

--- src/main/java/by/epam/krein/abitapp/controller/commandImpl/EditAdminSpecialtyInformation.java
+++ src/main/java/by/epam/krein/abitapp/controller/commandImpl/EditAdminSpecialtyInformation.java
@@ -6,27 +6,43 @@ import by.epam.krein.abitapp.entity.Exam;
 import by.epam.krein.abitapp.entity.Specialty;
 import by.epam.krein.abitapp.service.ServiceFactory;
 import by.epam.krein.abitapp.service.SpecialtyService;
+import by.epam.krein.abitapp.util.UniversityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 public class EditAdminSpecialtyInformation implements Command {
+
+    private final Logger logger = LoggerFactory.getLogger(EditAdminSpecialtyInformation.class);
 
     ServiceFactory serviceFactory = ServiceFactory.getInstance();
     SpecialtyService specialtyService = serviceFactory.getSpecialtyService();
 
     @Override
     public CommandName callCommandMethod(HttpServletRequest req) {
-        int id = Integer.parseInt(req.getParameter("id"));
-        List<Specialty> specialties = (List<Specialty>) req.getSession().getAttribute("specialties");
-        Specialty specialty = null;
-        for (Specialty specialtyExample : specialties){
-            if (specialtyExample.getId() == id) {
-                specialty = specialtyExample;
-                break;
-            }
+        String prepareId = req.getParameter("id");
+        if(UniversityUtils.checkId(prepareId)){ // Никогда не сработает, есть фильтр, хз, что лучше
+            logger.info("Failed, incorrect id");
+            return CommandName.ERROR.getCommand().callCommandMethod(req);
         }
+
+        int id = Integer.parseInt(prepareId);
+        List<Specialty> specialties = (List<Specialty>) req.getSession().getAttribute("specialties");
+        Specialty specialty = findSpecialty(specialties, id);
+
         req.setAttribute("exams", Exam.values());
         req.setAttribute("specialty", specialty);
         return CommandName.EDIT_ADMIN_SPECIALTY_INFORMATION;
+    }
+
+    private Specialty findSpecialty(List<Specialty> specialties, int specialtyId) {
+        for (Specialty specialty : specialties) {
+            if (specialty.getId() == specialtyId) {
+                return specialty;
+            }
+        }
+        return null;
     }
 }
