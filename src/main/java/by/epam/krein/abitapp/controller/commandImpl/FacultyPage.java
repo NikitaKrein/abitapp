4a81dac97adb64d0be2bfbb1af5fb0746b72f48f
Nik Krein <nikkrein1@gmail.com>
2020-07-27 01:45:14 +0300

    Refactor FacultyPage

--- src/main/java/by/epam/krein/abitapp/controller/commandImpl/FacultyPage.java
+++ src/main/java/by/epam/krein/abitapp/controller/commandImpl/FacultyPage.java
@@ -10,7 +10,10 @@ import by.epam.krein.abitapp.exception.CommandException;
 import by.epam.krein.abitapp.service.SpecialtyService;
 import by.epam.krein.abitapp.service.ServiceFactory;
 import by.epam.krein.abitapp.service.UniversityService;
+import by.epam.krein.abitapp.util.UniversityUtils;
 import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -19,60 +22,68 @@ import java.util.Map;
 
 public class FacultyPage implements Command {
 
+    private final Logger logger = LoggerFactory.getLogger(FacultyPage.class);
+
+
     ServiceFactory serviceFactory = ServiceFactory.getInstance();
     SpecialtyService specialtyService = serviceFactory.getSpecialtyService();
     UniversityService universityService = serviceFactory.getUniversityService();
 
-   // private final SpecialtyService facultyService = new SpecialtyServiceImpl(); // fabrika potom
-
     @Override
     public CommandName callCommandMethod(HttpServletRequest req) {
-        University faculty = null;
-        List<Pair<Integer, Pair<Integer, List<Pair<User, Integer>>>>> rating = new ArrayList<>();
-        List<Pair<Specialty, Boolean> > specialties = new ArrayList<>();
-        List<Specialty> prepareSpecialties = null;
-        int id;
-        StringBuilder prepareId = new StringBuilder(req.getPathInfo());
-        prepareId.deleteCharAt(0);
-        //prepareId.toString().matches("^/+\\d*")
-        id = Integer.parseInt(prepareId.toString());
+        University faculty;
+        List<Specialty> prepareSpecialties;
+        List<Pair<Integer, Pair<Integer, List<Pair<User, Integer>>>>> rating;
+        List<Pair<Specialty, Boolean>> specialties;
+        String prepareId = UniversityUtils.getId(req);
+
+        if(UniversityUtils.checkId(prepareId)){
+            logger.info("Failed, incorrect id in path.");
+            return CommandName.ERROR.getCommand().callCommandMethod(req);
+        }
+
+        int id = Integer.parseInt(prepareId);
+
         try {
             faculty = universityService.findUniversityById(id);
             prepareSpecialties = specialtyService.findSpecialtiesByFacultyId(id);
-        } catch(RuntimeException exception){
-            throw new CommandException("message", exception);
+            rating = UniversityUtils.getFacultyRating(prepareSpecialties);
+        } catch (RuntimeException exception) {
+            throw new CommandException("Faculty Page failed", exception);
         }
-        for (Specialty specialty : prepareSpecialties){
-            for (int i = 1; i < 5; i++){
-                try {
-                    rating.add(Pair.of(specialty.getId(), Pair.of(i, specialtyService.getSpecialtyRating(specialty.getId(), i))));
-                } catch(RuntimeException exception){
-                    throw new CommandException("message", exception);
-                }
-            }
-            if(canSubmitFaculty(req, specialty)){
-                specialties.add(Pair.of(specialty, true));
-            }
-            else{
-                specialties.add(Pair.of(specialty, false));
-            }
-        }
+
+        specialties = getSpecialtiesWithSubmitVerdict(req, prepareSpecialties);
         req.setAttribute("rating", rating);
         req.setAttribute("specialties", specialties);
         req.setAttribute("faculty", faculty);
         return CommandName.FACULTY_PAGE;
     }
-    private boolean canSubmitFaculty(HttpServletRequest req, Specialty specialty){
+
+
+    private List<Pair<Specialty, Boolean>> getSpecialtiesWithSubmitVerdict(HttpServletRequest req, List<Specialty> prepareSpecialties){
+        List<Pair<Specialty, Boolean>> specialties = new ArrayList<>();
+        for (Specialty specialty : prepareSpecialties) {
+            if (canSubmitFaculty(req, specialty)) {
+                specialties.add(Pair.of(specialty, true));
+            } else {
+                specialties.add(Pair.of(specialty, false));
+            }
+        }
+        return specialties;
+    }
+
+    private boolean canSubmitFaculty(HttpServletRequest req, Specialty specialty) {
         User user = checkUser(req);
-        if(user != null){
-            for (Exam facultyExam: specialty.getNameOfExams()){
+        if (user != null) {
+            for (Exam facultyExam : specialty.getNameOfExams()) {
                 boolean canSubmit = false;
-                for (Map.Entry<Exam, Integer> userExam : user.getExamMarks().entrySet()){
-                    if(userExam.getKey() == facultyExam){
+                for (Map.Entry<Exam, Integer> userExam : user.getExamMarks().entrySet()) {
+                    if (userExam.getKey() == facultyExam) {
                         canSubmit = true;
+                        break;
                     }
                 }
-                if(!canSubmit){
+                if (!canSubmit) {
                     return false;
                 }
             }
@@ -80,7 +91,8 @@ public class FacultyPage implements Command {
         }
         return false;
     }
-    private User checkUser(HttpServletRequest req){
+
+    private User checkUser(HttpServletRequest req) {
         User user = (User) req.getSession().getAttribute("user");
         if (req.getSession().getAttribute("user") != null) {
             return user;
