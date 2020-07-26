9f68adc8ef69f992016e9433d1ebe5b3c6d70515
Nik Krein <nikkrein1@gmail.com>
2020-07-26 16:02:04 +0300

    Refactor SignOutButton

--- src/main/java/by/epam/krein/abitapp/controller/commandImpl/SignOutButton.java
+++ src/main/java/by/epam/krein/abitapp/controller/commandImpl/SignOutButton.java
@@ -2,6 +2,7 @@ package by.epam.krein.abitapp.controller.commandImpl;
 
 import by.epam.krein.abitapp.controller.Command;
 import by.epam.krein.abitapp.controller.CommandName;
+import by.epam.krein.abitapp.entity.Admin;
 import by.epam.krein.abitapp.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,9 +14,24 @@ public class SignOutButton implements Command {
 
     @Override
     public CommandName callCommandMethod(HttpServletRequest req) {
-        User user = (User) req.getSession().getAttribute("user");
-        logger.info(user.getEmail() + " sign out");
+        if(req.getSession().getAttribute("user") != null){
+            signOutUser(req);
+        }
+        if(req.getSession().getAttribute("admin") != null){
+            signOutAdmin(req);
+        }
         req.getSession().invalidate();
         return CommandName.MAIN_PAGE;
     }
+
+    private void signOutUser(HttpServletRequest req){
+        User user = (User) req.getSession().getAttribute("user");
+        logger.info("User: " + user.getEmail() + " sign out");
+    }
+
+    private void signOutAdmin(HttpServletRequest req){
+        Admin admin = (Admin) req.getSession().getAttribute("admin");
+        logger.info("Admin: " + admin.getEmail() + " sign out");
+    }
+
 }
