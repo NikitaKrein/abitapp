2bb8ce2366c5cfb06edcaa2648ea7162b0bd7cd8
Nik Krein <nikkrein1@gmail.com>
2020-07-27 15:27:33 +0300

    Refactor EditAdminInformationButton

--- src/main/java/by/epam/krein/abitapp/controller/commandImpl/EditAdminInformationButton.java
+++ src/main/java/by/epam/krein/abitapp/controller/commandImpl/EditAdminInformationButton.java
@@ -4,11 +4,14 @@ import by.epam.krein.abitapp.controller.Command;
 import by.epam.krein.abitapp.controller.CommandName;
 import by.epam.krein.abitapp.entity.Admin;
 import by.epam.krein.abitapp.entity.Specialty;
+import by.epam.krein.abitapp.entity.University;
 import by.epam.krein.abitapp.exception.CommandException;
 import by.epam.krein.abitapp.service.AdminService;
 import by.epam.krein.abitapp.service.SpecialtyService;
 import by.epam.krein.abitapp.service.ServiceFactory;
 import by.epam.krein.abitapp.service.UniversityService;
+import by.epam.krein.abitapp.util.HttpUtils;
+import by.epam.krein.abitapp.util.UniversityUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -20,47 +23,40 @@ public class EditAdminInformationButton implements Command {
     SpecialtyService specialtyService = serviceFactory.getSpecialtyService();
     UniversityService universityService = serviceFactory.getUniversityService();
 
-//    private final SpecialtyService facultyService = new SpecialtyServiceImpl(); // fabrika potom
-//    private final UniversityService universityService = new UniversityServiceImpl();
-
     @Override
     public CommandName callCommandMethod(HttpServletRequest req) {
         Admin admin = (Admin) req.getSession().getAttribute("admin");
-        if (req.getParameter("button").equals("university")){
-            int id = Integer.parseInt(req.getParameter("id"));
-            String name = req.getParameter("name");
-            String information = req.getParameter("information");
-            try {
-                universityService.updateUniversity(name, information, id);
-            } catch(RuntimeException exception){
-                throw new CommandException("message", exception);
-            }
+        if (req.getParameter("button").equals("university")) {
+            editUniversityInformation(req);
         }
-        if(req.getParameter("button").equals("specialty")){
-            int id = Integer.parseInt(req.getParameter("id"));
-            String name = req.getParameter("name");
-            String information = req.getParameter("information");
-            int admissionPlanForFree = Integer.parseInt(req.getParameter("admissionPlanForFree"));
-            int admissionPlanForPaid = Integer.parseInt(req.getParameter("admissionPlanForPaid"));
-            int admissionPlanForCorrespondenceCourseForFree = Integer.parseInt(req.getParameter("admissionPlanForCorrespondenceCourseForFree"));
-            int admissionPlanForCorrespondenceCourseForPaid = Integer.parseInt(req.getParameter("admissionPlanForCorrespondenceCourseForPaid"));
-            try {
-                specialtyService.updateSpecialty(id, name, information, admissionPlanForFree, admissionPlanForPaid, admissionPlanForCorrespondenceCourseForFree, admissionPlanForCorrespondenceCourseForPaid);
-            } catch(RuntimeException exception){
-                throw new CommandException("message", exception);
-            }
+        if (req.getParameter("button").equals("specialty")) {
+            editSpecialtyInformation(req);
         }
+
         admin = adminService.findByEmail(admin.getEmail());
-        req.getSession().setAttribute("admin",admin);
-        if (admin.getUniversity().isFaculty()){
-            List<Specialty> specialties = null;
-            try {
-                specialties = specialtyService.findSpecialtiesByFacultyId(admin.getUniversity().getId());
-            } catch(RuntimeException exception){
-                throw new CommandException("message", exception);
-            }
-            req.getSession().setAttribute("specialties", specialties);
+        HttpUtils.updateSession(req, "admin", admin);
+        if (admin.getUniversity().isFaculty()) {
+            UniversityUtils.setSpecialities(req, admin.getUniversity().getId());
         }
         return CommandName.EDIT_ADMIN_INFORMATION_BUTTON;
     }
+
+    private void editUniversityInformation(HttpServletRequest req) {
+        int id = Integer.parseInt(req.getParameter("id"));
+        String name = req.getParameter("name");
+        String information = req.getParameter("information");
+        universityService.updateUniversity(name, information, id);
+    }
+
+    private void editSpecialtyInformation(HttpServletRequest req) {
+        int id = Integer.parseInt(req.getParameter("id"));
+        String name = req.getParameter("name");
+        String information = req.getParameter("information");
+        int admissionPlanForFree = Integer.parseInt(req.getParameter("admissionPlanForFree"));
+        int admissionPlanForPaid = Integer.parseInt(req.getParameter("admissionPlanForPaid"));
+        int admissionPlanForCorrespondenceCourseForFree = Integer.parseInt(req.getParameter("admissionPlanForCorrespondenceCourseForFree"));
+        int admissionPlanForCorrespondenceCourseForPaid = Integer.parseInt(req.getParameter("admissionPlanForCorrespondenceCourseForPaid"));
+        specialtyService.updateSpecialty(id, name, information, admissionPlanForFree, admissionPlanForPaid, admissionPlanForCorrespondenceCourseForFree, admissionPlanForCorrespondenceCourseForPaid);
+    }
+
 }
