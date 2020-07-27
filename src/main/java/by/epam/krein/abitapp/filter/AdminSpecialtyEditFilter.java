495a49383bcb6ca9817cd75ea9702ce4ee34200c
Nik Krein <nikkrein1@gmail.com>
2020-07-27 15:14:30 +0300

    Refactor AdminSpecialtyEditFFilter

--- src/main/java/by/epam/krein/abitapp/filter/AdminSpecialtyEditFilter.java
+++ src/main/java/by/epam/krein/abitapp/filter/AdminSpecialtyEditFilter.java
@@ -1,7 +1,11 @@
 package by.epam.krein.abitapp.filter;
 
-import by.epam.krein.abitapp.entity.Admin;
+import by.epam.krein.abitapp.controller.CommandName;
+import by.epam.krein.abitapp.controller.commandImpl.FacultyPage;
 import by.epam.krein.abitapp.entity.Specialty;
+import by.epam.krein.abitapp.util.UniversityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -11,13 +15,24 @@ import java.io.IOException;
 import java.util.List;
 
 public class AdminSpecialtyEditFilter implements Filter {
+
+    private final Logger logger = LoggerFactory.getLogger(AdminSpecialtyEditFilter.class);
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
         HttpSession session = req.getSession(false);
         List<Specialty> specialties = (List<Specialty>) session.getAttribute("specialties");
-        int id = Integer.parseInt(req.getParameter("id"));
+
+        String prepareId = req.getParameter("id");
+        if(UniversityUtils.checkId(prepareId)){
+            logger.info("Failed, incorrect id in req.");
+            resp.sendRedirect(req.getContextPath() + CommandName.ERROR.getJspAddress());
+            return;
+        }
+
+        int id = Integer.parseInt(prepareId);
         boolean flag = true;
         for (Specialty specialty : specialties){
             if (specialty.getId() == id){
